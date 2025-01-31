b50252680683661f97b467d53eeaab01a994dc32
haominhao <haominhao@baidu.com>
2025-01-31 10:30:56 +0800

    docs(README) feat(49-字母异位词分组)

---
+++ hmh/src/49.字母异位词分组.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode.cn id=49 lang=python3
+#
+# [49] 字母异位词分组
+#
+
+# @lc code=start
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = defaultdict(list)  # 创建一个空列表字典
+        for s in strs:
+            # 将s添加到对应的字母异位词key
+            d[''.join(sorted(s))].append(s)
+        return list(d.values())
+        
+# @lc code=end
+
